821a0b1e99d0b99b1bdbafb593bf504c583efb7b
Leo <34784689+Leomnz@users.noreply.github.com>
2023-12-14 18:59:51 -0600

    threaded_main.py records memory and time per move

--- threaded_main.py
+++ threaded_main.py
@@ -27,37 +27,63 @@ def simulate_games(num_games, game, player1, player2, thread_num, queue):
     X_wins = 0
     O_wins = 0
     Draws = 0
-    memory_usage = [None] * num_games
-    time_usage = [None] * num_games
+    memory_usage_player1 = []
+    time_usage_player1 = []
+    memory_usage_player2 = []
+    time_usage_player2 = []
     for i in range(0, num_games):
-        tracemalloc.start()
-        start = time.time()
         U3T.__init__()
-        eval = U3T.play_game_silent(player1, player2)
+
+        state = U3T.initial
+        eval = None
+        while eval is None:
+            # player 1
+            tracemalloc.start()
+            start = time.time()
+            move = player1(U3T, state)
+            state = U3T.result(state, move)
+            if U3T.terminal_test(state):
+                eval = U3T.utility(state, U3T.to_move(U3T.initial))
+            end = time.time()
+            memory_usage_player1.append(tracemalloc.get_traced_memory()[1])
+            time_usage_player1.append((end - start) * 10 ** 3)  # conversion for ms
+
+            # player 1
+            tracemalloc.clear_traces()
+            start = time.time()
+            move = player2(U3T, state)
+            state = U3T.result(state, move)
+            if U3T.terminal_test(state):
+                eval = U3T.utility(state, U3T.to_move(U3T.initial))
+            end = time.time()
+            memory_usage_player2.append(tracemalloc.get_traced_memory()[1])
+            tracemalloc.stop()
+            # print(i, tracemalloc.get_traced_memory())
+            time_usage_player2.append((end - start) * 10 ** 3)  # conversion for ms
+
         if eval >= 1:
             X_wins += 1
         elif eval <= -1:
             O_wins += 1
         else:
             Draws += 1
-        memory_usage[i] = tracemalloc.get_traced_memory()[1]
-        end = time.time()
-        tracemalloc.stop()
-        time_usage[i] = (end - start) * 10 ** 3  # conversion for ms
         if thread_num == 0:
             print(
-                f"Thread [{thread_num}], Game Number: [{i}], Eval: [{eval}], Memory Usage: [{memory_usage[i]}], Time Usage: [{time_usage[i]}]")
-    average_memory_usage = sum(memory_usage) / len(memory_usage)
-    average_time_usage = sum(time_usage) / len(time_usage)
+                f"Thread [{thread_num}], Game Number: [{i}], Eval: [{eval}], Memory Usage Player 1: [{memory_usage_player1[i]}], Memory Usage Player 2: [{memory_usage_player2[i]}],Time Usage Player 1: [{time_usage_player1[i]}], Time Usage Player 2: [{time_usage_player2[i]}]")
+    average_memory_usage_player1 = sum(memory_usage_player1) / len(memory_usage_player1)
+    average_memory_usage_player2 = sum(memory_usage_player2) / len(memory_usage_player2)
+    average_time_usage_player1 = sum(time_usage_player1) / len(time_usage_player1)
+    average_time_usage_player2 = sum(time_usage_player2) / len(time_usage_player2)
     # results[thread_num] = (X_wins, O_wins, Draws, average_memory_usage, average_time_usage)
     # print(f"Thread [{thread_num}], X wins: [{X_wins}], O wins: [{O_wins}], Draws: [{Draws}], Average Memory Usage: [{average_memory_usage}], Average Time Usage: [{average_time_usage}]")
-    queue.put((X_wins, O_wins, Draws, average_memory_usage, average_time_usage))
+    queue.put((X_wins, O_wins, Draws, average_memory_usage_player1, average_memory_usage_player2,
+               average_time_usage_player1, average_time_usage_player2))
 
 
 if __name__ == "__main__":
     thread_count = 20
     threads = [None] * thread_count
-    total_games = 500
+    total_games = 100
     games_per_thread = total_games // thread_count
     print("Games per thread: ", games_per_thread)
     print("Total games: ", total_games)
@@ -66,7 +92,7 @@ if __name__ == "__main__":
     for i in range(0, thread_count):
         threads[i] = multiprocessing.Process(target=simulate_games,
                                              args=(
-                                                 games_per_thread, U3T(), heatmap_player, alpha_beta_cutoff_player,
+                                                 games_per_thread, U3T(), games.random_player, games4e.mcts_player,
                                                  i, queue))
     for i in range(0, thread_count):
         threads[i].start()
@@ -79,22 +105,28 @@ if __name__ == "__main__":
     Total_X_wins = 0
     Total_O_wins = 0
     Total_Draws = 0
-    Total_Memory_Usage = [0] * thread_count
-    Total_Time_Usage = [0] * thread_count
-    Average_Memory_Usage = 0
-    Average_Time_Usage = 0
+    Total_Memory_Usage_player1 = [0] * thread_count
+    Total_Memory_Usage_player2 = [0] * thread_count
+    Total_Time_Usage_player1 = [0] * thread_count
+    Total_Time_Usage_player2 = [0] * thread_count
     for element in range(0, queue.qsize()):
-        X_wins, O_wins, Draws, average_memory_usage, average_time_usage = queue.get()
+        X_wins, O_wins, Draws, average_memory_usage_player1, average_memory_usage_player2, average_time_usage_player1, average_time_usage_player2 = queue.get()
         Total_X_wins += X_wins
         Total_O_wins += O_wins
         Total_Draws += Draws
-        Total_Memory_Usage[element] = average_memory_usage
-        Total_Time_Usage[element] = average_time_usage
+        Total_Memory_Usage_player1[element] = average_memory_usage_player1
+        Total_Memory_Usage_player2[element] = average_memory_usage_player2
+        Total_Time_Usage_player1[element] = average_time_usage_player1
+        Total_Time_Usage_player2[element] = average_time_usage_player2
 
-    Average_Time_Usage = sum(Total_Time_Usage) / len(Total_Time_Usage)
-    Average_Memory_Usage = sum(Total_Memory_Usage) / len(Total_Memory_Usage)
+    Average_Time_Usage_player1 = sum(Total_Time_Usage_player1) / len(Total_Time_Usage_player1)
+    Average_Time_Usage_player2 = sum(Total_Time_Usage_player2) / len(Total_Time_Usage_player2)
+    Average_Memory_Usage_player1 = sum(Total_Memory_Usage_player1) / len(Total_Memory_Usage_player1)
+    Average_Memory_Usage_player2 = sum(Total_Memory_Usage_player2) / len(Total_Memory_Usage_player2)
     print("Total X wins: ", Total_X_wins)
     print("Total O wins: ", Total_O_wins)
     print("Total Draws: ", Total_Draws)
-    print("Average Memory Usage: ", Average_Memory_Usage / 1000, "KB")
-    print("Average Time Usage: ", Average_Time_Usage / 1000, "s")
+    print("Average Memory Usage Player 1: ", Average_Memory_Usage_player1 / 1000, "KB")
+    print("Average Memory Usage Player 2: ", Average_Memory_Usage_player2 / 1000, "KB")
+    print("Average Time Usage Player 1: ", Average_Time_Usage_player1 / 1000, "s")
+    print("Average Time Usage Player 2: ", Average_Time_Usage_player2 / 1000, "s")
